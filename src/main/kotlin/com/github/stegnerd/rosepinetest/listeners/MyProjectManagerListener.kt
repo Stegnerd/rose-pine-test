8b11fc78e4af325a2c1e01840eef21a65ad9ff55
GitHub Action <action@github.com>
2021-11-13 21:10:39 +0000

    Template cleanup

--- src/main/kotlin/com/github/stegnerd/rosepinetest/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/stegnerd/rosepinetest/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.stegnerd.rosepinetest.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.stegnerd.rosepinetest.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/stegnerd/rosepinetest/services/MyProjectService.kt
+++ src/main/kotlin/com/github/stegnerd/rosepinetest/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.stegnerd.rosepinetest.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.stegnerd.rosepinetest.MyBundle
 
 class MyProjectService(project: Project) {
 
